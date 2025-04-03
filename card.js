23164f07c97c79426cde9c53d025814f90b92421
mithilesh5 <mithileshbawane.mb@gmail.com>
2025-04-03 20:58:52 +0530

    Delete card.js

--- card.js
+++
@@ -1,7 +0,0 @@
-export function Card({ children }) {
-  return <div className="p-6 bg-gray-800 rounded-lg shadow-lg">{children}</div>;
-}
-
-export function CardContent({ children }) {
-  return <div className="mt-4">{children}</div>;
-}
